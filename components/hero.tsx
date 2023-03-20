03fa6412a6f11f8f31b0d1eb980400f9f1c25c69
Priyanshu Singh <reveur.guy99@gmail.com>
2023-03-20 17:49:53 +0530

    color change

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,7 +4,7 @@ const Hero: NextPage = () => {
   return (
     <div className="relative">
       <div className="mx-auto max-w-md px-6 sm:max-w-3xl lg:max-w-7xl lg:px-8">
-        <div className="relative overflow-hidden rounded-2xl bg-[#12a3e3] px-6 py-10 shadow-xl sm:px-12 sm:py-20">
+        <div className="relative overflow-hidden rounded-2xl bg-[#0f4db2] px-6 py-10 shadow-xl sm:px-12 sm:py-20">
           <div aria-hidden="true" className="absolute inset-0 -mt-72 sm:-mt-32 md:mt-0">
             <svg
               className="absolute inset-0 h-full w-full"
@@ -14,12 +14,12 @@ const Hero: NextPage = () => {
               viewBox="0 0 1463 360"
             >
               <path
-                className="text-[#9ceb] text-opacity-40"
+                className="text-[#54c5f6] text-opacity-40"
                 fill="currentColor"
                 d="M-82.673 72l1761.849 472.086-134.327 501.315-1761.85-472.086z"
               />
               <path
-                className="text-[#9ceb] text-opacity-40"
+                className="text-[#54c5f6] text-opacity-40"
                 fill="currentColor"
                 d="M-217.088 544.086L1544.761 72l134.327 501.316-1761.849 472.086z"
               />
@@ -34,10 +34,10 @@ const Hero: NextPage = () => {
                 A community of Python programmers who are dedicated to empowering and recognizing contributors from around the world. Our mission is to create a collaborative and inclusive environment where Python enthusiasts of all skill levels can come together to share knowledge, work on projects, and grow their skills.
               </p>
             </div>
-            <div className="mt-4 justify-center flex items-center scale-110">
+            <div className="mt-6 justify-center flex items-center scale-110">
               <button
                 type="submit"
-                className="rounded-md bg-blue-600 px-3.5 py-2 text-lg font-bold leading-7 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                className="rounded-md bg-blue-300 px-3.5 py-2 text-lg font-bold leading-7 text-black shadow-sm hover:bg-blue-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-300"
               >
                 Get Started
               </button>
